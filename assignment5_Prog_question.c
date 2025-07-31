26bc51c94daa10a9c3575af18c352fd3229ac8d9
mcblanton2 <75965683+mcblanton2@users.noreply.github.com>
2025-07-31 14:28:34 -0500

    Add files via upload

--- assignment5_Prog_question.c
+++ assignment5_Prog_question.c
@@ -3,33 +3,37 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-int counter = 0;            // Shared counter
-sem_t mutex;                // Binary semaphore
-
-void* t1_func(void* arg) {
-    while (1) {
-        sem_wait(&mutex);       // P()
+int counter = 0;
+sem_t mutex;
+void *t1_func(void *arg)
+{
+    while (1)
+    {
+        sem_wait(&mutex);
         counter++;
         printf("t1 incremented counter: %d\n", counter);
-        sem_post(&mutex);       // V()
+        sem_post(&mutex);
         sleep(1);
     }
 }
 
-void* t2_func(void* arg) {
-    while (1) {
-        sem_wait(&mutex);       // P()
+void *t2_func(void *arg)
+{
+    while (1)
+    {
+        sem_wait(&mutex);
         counter--;
         printf("t2 decremented counter: %d\n", counter);
-        sem_post(&mutex);       // V()
+        sem_post(&mutex);
         sleep(1);
     }
 }
 
-int main() {
+int main()
+{
     pthread_t t1, t2;
 
-    sem_init(&mutex, 0, 1);     // Initialize semaphore to 1 (unlocked)
+    sem_init(&mutex, 0, 1);
 
     pthread_create(&t1, NULL, t1_func, NULL);
     pthread_create(&t2, NULL, t2_func, NULL);
@@ -37,6 +41,6 @@ int main() {
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
 
-    sem_destroy(&mutex);        // Cleanup
+    sem_destroy(&mutex);
     return 0;
 }
